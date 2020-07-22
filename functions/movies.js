760f60e663dc614ff0172df0e5946ba75c9ab8b6
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-23 00:13:04 +0530

    Hasura connect!

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,21 @@
 const { URL } = require("url");
 const fetch = require("node-fetch");
-const movies = require("../data/movies.json");
+const { query } = require("./util/hasura");
 
 exports.handler = async () => {
+  const { movies } = await query({
+    query: `
+      query readAllMovies {
+        movies {
+          id
+          poster
+          tagline
+          title
+        }
+      }
+    `,
+  });
+
   const api = new URL("https://www.omdbapi.com/");
   api.searchParams.set("apiKey", process.env.OMDB_API_KEY);
   const moviesWithApi = movies.map((movie) => {
